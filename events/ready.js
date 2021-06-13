10e96f177e222b6979b75b9fb1eac2bb2a8ba057
zKemer <76616120+zKemer@users.noreply.github.com>
2021-06-13 11:56:41 +0300

    .

--- events/ready.js
+++ events/ready.js
@@ -1,33 +1,22 @@
-const chalk = require("chalk");
-const moment = require("moment");
-const Discord = require("discord.js");
-const ayarlar = require("../ayarlar.json");
-const db = require('croxydb'); // creating database
-
+const Discord = require('discord.js');
+const moment = require('moment');
+const chalk = require('chalk');
+const { prefix } = require('../ayarlar.json')
 
 module.exports = client => {
-  console.log(
-    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: Aktif, Komutlar Yüklendi!`
-  );
-  console.log(
-    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: ${
-      client.user.username
-    } İsmi ${client.user.id} ID'si ile Sisteme Giriş Yapıldı!`
-  );
+  var degisenOynuyor = [
+    
+    "DEĞİŞEN OYNUYOR 1",   //İSTEDİĞİNİZ KADAR YAPABİLİRSİNİZ SADECE SONRA , KOYMAYIN
+    "DEĞİŞEN OYNUYOR 2"
+    
+  ]
   
-  var oyun = [
-        "idle", "dnd"
-    ];
-  client.user.setActivity(db.fetch(`presence_status`))
+  setInterval(function() {
+    var degisenOynuyor1 = degisenOynuyor[Math.floor(Math.random() * (degisenOynuyor.length))]
+    client.user.setActivity(`${degisenOynuyor1}`);
 
-    setInterval(function() {
-
-        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
-
-        client.user.setStatus(oyun[random]);
-        }, 5000);
+}, 2 * 30000);
   
-
-
-
-};
+  client.user.setStatus("online"); //dnd, idle, online, offline
+  
+}
--- util/error.js
+++
@@ -1,7 +0,0 @@
-const { MessageEmbed } = require("discord.js")
-module.exports = async (text, channel, message) => {
-    let embed = new MessageEmbed()
-    .setColor("#F0FFFF")
-    .setDescription(text)
-    await channel.send(embed)
-}
